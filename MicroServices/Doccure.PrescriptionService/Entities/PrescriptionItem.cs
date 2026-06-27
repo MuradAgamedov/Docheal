8460733140c9523d3745c972a512e154427cee07
Murad <agamedov94@mail.ru>
2026-06-27 19:51:41 +0400

    Install Npgsql.EntityFrameworkCore.PostgreSQL version 9.0.0 package

---
+++ MicroServices/Doccure.PrescriptionService/Entities/PrescriptionItem.cs
@@ -0,0 +1,12 @@
+﻿namespace Doccure.PrescriptionService.Entities
+{
+    public class PrescriptionItem
+    {
+        public int PrescriptionItemId { get; set; }
+        public string MedicineName { get; set; }
+        public string Usage { get; set; } 
+        public string Duration { get; set; } 
+        public int PrecriptionId { get; set; }
+        public Prescription Prescription { get; set; }
+    }
+}
